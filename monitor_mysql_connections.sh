2e18615f2cca2a4a82d707d80c3c02690ca38b86
Tony Scholes <tonys@talisman.tech>
2021-09-24 14:07:52 +0100

    Changed server name

--- monitor_mysql_connections.sh
+++ monitor_mysql_connections.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-set -vx
+#
+# Monitor current MySQL connections, send an email to the support system if a given threshold is exceeded.
+#
+
+#set -vx
 
 # Declare some global variables
-hostname=talisman-web22.com
+hostname=mi-man-talsql1
 threshold_connections=${1:-200}
 check_frequency=${3:-300}
-mail_max_frequency=3600
+mail_max_frequency=${4:-3600}
 mail_last_sent=0
-mail_recipients=${2:-tonys@talisman.tech}
+mail_recipients=${2:-help@support.talisman.tech}
+mail_subject="MySQL Connections monitor on $hostname"
 
+#
+# Send alert mail, not too frequently, avoid flooding support ticket system.
+#
 send_alert_mail() {
 	now=`date +%s`
 	if [ `expr $now - $mail_last_sent` -gt $mail_max_frequency ]
 	then
 		mail_last_sent=$now
-		echo "Only $1 Connection slots free at `date`.\n\nKInd Regards" | mutt -s "MySQL Connections monitor on $hostname" $mail_recipients
+		EMAIL=talisman_tech@talisman-web22.com export EMAIL
+		echo "Hi,\n\nThe MySQL Connections Monitor in $hostname reports only $1 connections free at `date`.\n\nThere are $2 connections in use of $3 maximum concurrent connections\n\nKind Regards" | mutt -s "$mail_subject" $mail_recipients
 	fi
 }
 
@@ -28,7 +37,7 @@ do
 
 	if [ $free_connections -lt $threshold_connections ] 
 	then
-		send_alert_mail $free_connections
+		send_alert_mail $free_connections $current_connections $max_connections
 	fi
 
 	sleep $check_frequency
